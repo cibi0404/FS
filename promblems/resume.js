5c51444466b610bcc9d5b9c292d126904782561f
parthasarathi m <parthasarathim@parthasarathis-MacBook-Air.local>
2024-02-07 15:14:09 +0530

    day5 taks added

---
+++ promblems/resume.js
@@ -0,0 +1,48 @@
+let myResume = {
+ "basics": {
+  "name":  "PARTHASARATHI M",
+  "email":  "parthasarathi0404@gamil.com",
+  "degree":  "B.E",
+  "phone":  7358975066,
+  "location":{
+    "address":  "6/5 mela veethi ponparappi",
+   "pincode":  "621710",
+   "town":  "ARIYALUR",
+   "state":  "TAMILNADU", 
+   "country": "INDIA",
+  },
+  "profile":[
+    {
+      "website": "https://www.linkedin.com/feed/",
+      "github": "https://github.com/cibi0404/FS",
+    }
+  ]
+},
+"education":[
+  {
+    "collage": "SRM VALLIAMMAI ENGINEERING COLLAGE",
+    "department": "ECE",
+    "gpa": 7.81,
+
+  }
+],
+"skills":[
+  {
+    "name": "javascript",
+    
+  }
+],
+"language": [
+  { 
+    "known languages":  "english,tamil",
+
+  }
+],
+"hobbies":[
+  {
+    "type":  "playing cricket",
+  }
+]      
+ 
+}
+console.log(myResume);
